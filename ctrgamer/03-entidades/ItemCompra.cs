ad0424af49d6941df6824fda039504ec2014e922
Victorhugorodriguescost <163910434+victorhrc242@users.noreply.github.com>
2024-09-10 17:04:27 -0300

    10/09/2024
    Compra
    
    2. ItemCompra
    3. Categoria
    4. Pagamento
    5. JogoCategoria
    6. Revendedor
    7. Avaliação
    implementei essas novas classes agora teos 10 classes que ja tem suas propriedades com cada um relacionadas  certo e vai ficar faltando so  fazer um crud para elas ficarem salavas no banco de dados e terminarei a aula de hj!

---
+++ ctrgamer/03-entidades/ItemCompra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctrgamer._03_entidades
+{
+    public class ItemCompra
+    {
+        public int itemcompraid { get; set; }
+        public int Compraid { get; set; }
+        public int jogoid { get; set; }
+        public int quantidade { get; set; }
+        public decimal precounutario { get; set; }
+        public decimal totalitem { get; set; }
+    }
+}
