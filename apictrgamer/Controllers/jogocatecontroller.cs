e294b4f082d94074fed3ec236ec8bcc85efca67c
Victorhugorodriguescost <163910434+victorhrc242@users.noreply.github.com>
2024-09-11 15:08:57 -0300

    Reapply "11/09/2024 codigo com erro"
    This reverts commit 0c7e8d7f27ba3b7081fa9dbed92eca944f016081.

---
+++ apictrgamer/Controllers/jogocatecontroller.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using ctrgamer._01_service;
+using ctrgamer._03_entidades.DTO.usuario;
+using ctrgamer._03_entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apictrgamer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class jogocatecontroller:ControllerBase
+    {
+        private readonly jogocategoriaservice service;
+        private readonly IMapper mapper;
+        public jogocatecontroller(IMapper _mapper, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            service = new jogocategoriaservice(connectionString);
+            _mapper = mapper;
+
+        }
+
+        [HttpPost("adicionar-usuario")]
+        public void adicionaraluno(JogoCategoria u)
+        {
+            JogoCategoria usuario = mapper.Map<JogoCategoria>(u);
+            service.Adicionar(usuario);
+        }
+
+        [HttpGet("Listar-aluno")]
+        public List<JogoCategoria> Listaraluno()
+        {
+            return service.Listar();
+        }
+        [HttpDelete("Remover-aluno")]
+        public void Removeralunoaluno(int id)
+        {
+            service.Remover(id);
+        }
+        [HttpPut("editar-aluno")]
+        public void editaraluno(JogoCategoria usuario)
+        {
+            service.editar(usuario);
+        }
+    }
+}
---
+++ ctrgamer/02-Repositorio/revendedorrepositor.cs
@@ -0,0 +1,55 @@
+﻿using ctrgamer._03_entidades;
+using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctrgamer._02_Repositorio
+{
+    public class revendedorrepositor
+    {
+        private readonly string ConnectionString;
+        public revendedorrepositor(string connectionString)
+        {
+
+            ConnectionString = connectionString;
+
+        }
+        public void Adicionar(Reevendedor r)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            connection.Insert<Reevendedor>(r);
+
+        }
+        public List<Reevendedor> listar()
+        {
+            {
+                using var connection = new SQLiteConnection(ConnectionString);
+                {
+                    List<Reevendedor> u = connection.GetAll<Reevendedor>().ToList();
+                    return u;
+                }
+            }
+        }
+        public Reevendedor Buscarporid(int id)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            return connection.Get<Reevendedor>(id);
+        }
+        public void Remover(int id)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+            Reevendedor novoproduto = Buscarporid(id);
+            connection.Delete<Reevendedor>(novoproduto);
+        }
+        public void editar(Reevendedor r)
+        {
+            using var connection = new SQLiteConnection(ConnectionString);
+
+            connection.Update<Reevendedor>(r);
+        }
+    }
+}
