e2fc3217dc90287248eef7af170ff48166fe56e4
Victorhugorodriguescost <163910434+victorhrc242@users.noreply.github.com>
2024-11-27 13:55:18 -0300

    27/11/2024   apaguei o fronteend para fazer outro

--- Front-End-ADM/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Front_End_ADM.Inicio;
-HttpClient cliente = new HttpClient
-{
-    BaseAddress = new Uri("https://localhost:7007/")
-};
-Sistema s=new Sistema(cliente);
-s.iniciarsistema();
--- Front-end/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using FrontEnd.inicio;
-HttpClient cliente = new HttpClient
-{
-    BaseAddress = new Uri("https://localhost:7007/")
-};
-sistema s=new sistema(cliente);
-s.iniciarsistema();
